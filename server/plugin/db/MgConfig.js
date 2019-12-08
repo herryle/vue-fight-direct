148cf2cc88ad5242ec39ef303409bf8ed0c3f87c
herryle <herryle93@gmail.com>
2019-12-08 13:51:08 +0800

    配置修改

--- server/plugin/db/MgConfig.js
+++ server/plugin/db/MgConfig.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   const mongoose = require('mongoose')
-  mongoose.connect('mongodb://loclhost:27018/vue-fight-direct', {
+  mongoose.connect('mongodb://127.0.0.1:27018/vue-fight-direct', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
